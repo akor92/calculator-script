c9bde7fbee5b4657920c91fc2e9b75f5fb7bf28b
akor92 <akorinnocent@protonmail.com>
2024-07-31 10:55:48 +0100

    first commit

---
+++ calculator.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+read -p "enter your name" name
+read -p "enter your state" state
+read -p "enter your year of birth" year_of_birth
+current_year=2024
+age=$((current_year - year_of_birth))
+echo "your name is $name and you from $state state, you are $age years old"
+
